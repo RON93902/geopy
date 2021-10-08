6c0e6ec1b290769347077a999a8e095912ddcef6
kevin.stanton <kevin.stanton@mottmac.com>
2021-10-08 09:59:53 -0700

    update documentation

--- wall-pressure-calcs.py
+++ wall-pressure-calcs.py
@@ -1,19 +1,53 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun 26 13:32:51 2018
-
 @author: kevin.stanton
 
-Notes:
-    phi = effective friction angle
-    c = cohesion
-    PGA = peak ground acceleration (g)
-    alpha = orientation of the failure surface
-    beta = slope of the wall on the backfill side (angle with vertical)
-    i = backfill slope (angle with horizontal)
-    q = surcharge
-    hvRatio = ratio of ha to hv
-    delta = friction angle between wall and soil
+This function computes active and passive seismic coeffients for a retaining wall. 
+The orientation of the acceleration field is determined based on PGA and the ratio
+of ha to hv. The script automatically analyzed a range of failure surface angles 
+and returns the angle and associated values for the case with the lowest computed
+safety factor. 
+
+Parameters
+        ----------
+            phi : float
+                - effective friction angle (degrees)
+            c : float
+                - cohesion (kPa)
+            PGA : float
+                - peak ground acceleration (g)
+            gamma : float (optional)
+                - unit weight of soil (kN/m^3)
+                - Only require if c>0 (default=0)
+            H : float (optional)
+                - height of cohesive zone (m)
+                - default = 0
+            beta : float (optional)
+                - angle against vertical of the slope of the wall on the backfill side (degrees)
+            i : float (optional)
+                - angle with horizontal backfill slope (degrees)
+                - default = 0
+            q : str (optional)
+                - surcharge (kPa)
+            hvRatio : float (optional)
+                - ratio of ha to hv 
+                - default = 0
+            delta : float (optional)
+                - friction angle between soil and wall (degrees)
+                - default = (2/3)*phi
+            
+        Returns
+        -------
+            Kae : float
+                - active earth pressure coeffient associated with the governing failure surface angle
+            alphaFa : float
+                - governing failure surface angle for the active case (degrees)
+            Kpe : float
+                - active earth pressure coeffient associated with the governing failure surface angle
+            alphaFp : float
+                - governing failure surface angle for the active case (degrees)
+            theta : float
+                - orientation of the applied acceleration field with respect to horizontal (degrees)
 """
 
 
@@ -33,7 +67,6 @@ def Kae(phi, c, PGA, gamma=0, H=0, beta = 0, i = 0, q = 0, hvRatio = 0, delta = 
     i = np.deg2rad(i)
     phi = np.deg2rad(phi)
     delta = np.deg2rad(delta)
-#    theta = np.deg2rad(theta)
     
     if delta == 0:
         delta = 2*phi/3
@@ -51,7 +84,7 @@ def Kae(phi, c, PGA, gamma=0, H=0, beta = 0, i = 0, q = 0, hvRatio = 0, delta = 
         
         alpha = np.deg2rad(x)
         
-        # Compute the active lateral seismic coefficient 
+        # Compute the active and passive lateral seismic coefficients 
         Kae1 = np.sin(alpha-phi+theta)*np.cos(alpha-beta)*(np.cos(beta-i)+2*q*np.cos(beta)/(gamma*H*(1-kv)))\
             /((np.cos(beta)**2)*np.cos(theta)*np.sin(alpha-i)*np.cos(alpha-beta-phi-delta))\
             -2*c*np.cos(beta-i)*np.cos(phi)/(gamma*H*(1-kv)*np.cos(beta)*np.sin(alpha-i)*np.cos(alpha-beta-phi-delta))
